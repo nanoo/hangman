724527bf68100e8d57658a0c8bac5788215be16b
nanoo <tempi@mac.com>
2020-10-24 11:43:27 -0400

    feat: Add initial board state. (#2)

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import random
+HANGMAN_PICS = ['''
+  +---+
+      |
+      |
+      |
+     ===''']
+
 words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar' \
         ' coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk' \
         ' lion lizard llama mole monkey moose mouse mule newt otter owl panda' \
@@ -12,7 +19,16 @@ def getRandomWord(wordList):
     wordIndex = random.randint(0, len(wordList) - 1)
     return wordList[wordIndex]
 
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_PICS[len(missedLetters)])
+
+
 print('H A N G M A N')
 missedLetters = ''
 correctLetters = ''
 secretWord = getRandomWord(words)
+gameIsDone = False
+
+while True:
+    displayBoard(missedLetters, correctLetters, secretWord)
+    break
