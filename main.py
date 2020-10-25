772c29413dd67dc827cb381d305229a77198de1d
nanoo <tempi@mac.com>
2020-10-25 12:49:48 -0400

    feat: Finish the 'displayBoard' function. (#3)
    Now, the guessed letters are shown.

--- main.py
+++ main.py
@@ -19,8 +19,25 @@ def getRandomWord(wordList):
     wordIndex = random.randint(0, len(wordList) - 1)
     return wordList[wordIndex]
 
+
 def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_PICS[len(missedLetters)])
+    print()
+
+    print('Missed letters:', end=' ')
+    for letter in missedLetters:
+        print(letter, end=' ')
+    print()
+
+    blanks = '_' * len(secretWord)
+
+    for i in range(len(secretWord)):  # Replace blanks with correctly guessed letters.
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks:  # Show the secret word with spaces in between each letter.
+        print(letter, end=' ')
+    print()
 
 
 print('H A N G M A N')
